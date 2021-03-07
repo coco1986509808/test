c83e2ccbae7035e04a2772ae7dcbc194ca640cad
coco1986509808 <1986509808@qq.com>
2021-03-07 20:44:14 +0800

    must

--- eclipse_git/src/eclipse_git/Eclipse.java
+++ eclipse_git/src/eclipse_git/Eclipse.java
@@ -4,7 +4,7 @@ public class Eclipse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello eclipse");
+System.out.println("hello eclipseq");
 	}
 
 }
